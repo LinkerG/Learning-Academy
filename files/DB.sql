68c89acfc98f6661cc72a55b2a57bea324aefd25
Iker Gonzalez Tirado <52651951+LinkerG@users.noreply.github.com>
2023-09-18 13:36:57 +0200

    Rollback del archivo de sql

--- files/DB.sql
+++ files/DB.sql
@@ -53,35 +53,3 @@ CREATE TABLE users(
 
 INSERT INTO users (email, password, role)
 VALUES ('super@admin.com', MD5('administrador'), 'A');
-
--- Modificar la columna "dni" para permitir NULL
-ALTER TABLE users
-MODIFY COLUMN dni varchar(9) DEFAULT NULL;
-
--- Establecer el valor predeterminado del campo "dni" para usuarios no administradores ('S' o 'T')
-UPDATE users
-SET dni = ''
-WHERE role IN ('S', 'T');
-
--- Agregar una restricción para garantizar que "dni" sea NULL solo para usuarios administradores
-ALTER TABLE users
-ADD CONSTRAINT chk_admin_dni
-CHECK ((role = 'A' AND dni IS NULL) OR (role IN ('S', 'T') AND dni IS NOT NULL));
-
--- Agregar clave foránea para usuarios estudiantes ('S')
-ALTER TABLE users
-ADD CONSTRAINT fk_student_dni
-FOREIGN KEY (dni)
-REFERENCES student(dniStudent)
-ON DELETE CASCADE
-ON UPDATE CASCADE
-WHERE role = 'S';
-
--- Agregar clave foránea para usuarios profesores ('T')
-ALTER TABLE users
-ADD CONSTRAINT fk_teacher_dni
-FOREIGN KEY (dni)
-REFERENCES teacher(dniTeacher)
-ON DELETE CASCADE
-ON UPDATE CASCADE
-WHERE role = 'T';
