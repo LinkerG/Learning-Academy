67892efa20f3a2bdcddca00ef93f99fd8e929e62
Iker Gonzalez Tirado <linkervideogames@gmail.com>
2023-09-18 20:11:56 +0200

    Formulario para añadir courso (solo vista, no funciona)

--- admin/addCourse.php
+++ admin/addCourse.php
@@ -1,11 +1,35 @@
+<?php
+    session_start();
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Add new course</title>
+    <link rel="stylesheet" href="../css/style.css">
 </head>
 <body>
-    añadir
+    <?php
+        if(!isset($_SESSION['role']) || $_SESSION['role'] != "A") {
+            include("needAdmin.html");
+            header("Refresh: 5; URL='close.php'");
+            exit;
+        }
+    ?>
+    <form action="#">
+        <label for="name">Name:</label>
+        <input type="text" name="name" id="name">
+        <label for="hours">Hours:</label>
+        <input type="number" name="hours" id="hours">
+        <label for="start">Start date:</label>
+        <input type="date" name="start" id="start">
+        <label for="end">End date:</label>
+        <input type="date" name="end" id="end">
+        <label for="teacher">Teacher DNI:</label>
+        <input type="text" name="teacher" id="teacher">
+        <input type="submit" value="Add">
+    </form>
+    <a href="courses.php">Cancel</a>
 </body>
 </html>
