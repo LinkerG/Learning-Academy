89572f67e27003a64c9d5cf6562a0475b34e5892
Iker Gonzalez Tirado <linkervideogames@gmail.com>
2023-09-18 23:31:21 +0200

    teachers

--- admin/addTeacher.php
+++ admin/addTeacher.php
@@ -6,7 +6,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Add new course</title>
+    <title>Add new teacher</title>
     <link rel="stylesheet" href="../css/style.css">
 </head>
 <body>
@@ -26,18 +26,22 @@
         }
     ?>
     <form action="#" method="POST">
+        <label for="dni">DNI:</label>
+        <input type="text" name="dni" id="dni">
         <label for="name">Name:</label>
         <input type="text" name="name" id="name">
-        <label for="hours">Hours:</label>
-        <input type="number" name="hours" id="hours">
-        <label for="start">Start date:</label>
-        <input type="date" name="start" id="start">
-        <label for="end">End date:</label>
-        <input type="date" name="end" id="end">
-        <label for="teacher">Teacher DNI:</label>
-        <input type="text" name="teacher" id="teacher">
+        <label for="surname">Surname:</label>
+        <input type="text" name="surname" id="surname">
+        <label for="email">Email:</label>
+        <input type="email" name="email" id="email">
+        <label for="password">Password:</label>
+        <input type="password" name="password" id="password">
+        <label for="titulation">Titulation:</label>
+        <input type="text" name="titulation" id="titulation">
+        <label for="file">Photo:</label>
+        <input type="file" name="file" id="file">
         <input type="submit" value="Add">
     </form>
-    <a href="courses.php">Cancel</a>
+    <a href="teachers.php">Cancel</a>
 </body>
 </html>
---
+++ admin/teachers.php
@@ -0,0 +1,60 @@
+<?php
+    session_start();
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Manage teachers</title>
+    <link rel="stylesheet" href="../css/style.css">
+</head>
+<body>
+    <?php
+        if(!isset($_SESSION['role']) || $_SESSION['role'] != "A") {
+            include("needAdmin.html");
+            header("Refresh: 5; URL='close.php'");
+            exit;
+        }
+    ?>
+    <h1>Teacher list</h1>
+    <div class="listContainer">
+        <div class="tableToolbar">
+            <a class="linkButton" href="addTeacher.php">Add new teacher</a>
+        </div>
+
+        <table>
+            <tr>
+                <th>DNI</th>
+                <th>Name</th>
+                <th>Surname</th>
+                <th>Email</th>
+                <th>Password (MD5)</th>
+                <th>Edit</th>
+                <th>Disabled</th>
+            </tr>
+            <?php
+                include("../functions.php");
+
+                if(connectBD("learningacademy", $connection)) {
+                    $sql = "SELECT * FROM course";
+
+                    if(selectSQL($connection, $sql, $result)){
+                        if(empty($result)) {
+                            echo "<tr>";
+                            echo "<td colspan='6'>There are no courses right now</td>";
+                            echo "</tr>";
+                        } else {
+                            foreach($result as $row) {
+                                echo "<tr>";
+                                
+                                echo "</tr>";
+                            }
+                        }
+                    }
+                }
+            ?>
+        </table>
+    </div>
+</body>
+</html>
