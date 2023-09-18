0566caec8c6e1e07dbfd0b43c23ffece764eb8b5
Iker Gonzalez Tirado <linkervideogames@gmail.com>
2023-09-18 13:51:07 +0200

    Arreglo en BD y functions

--- functions.php
+++ functions.php
@@ -68,7 +68,7 @@
             // Cambiar el nombre de la bd segun convenga
             if(connectBD("learningacademy", $connection)){
                 // Cambiar el nombre de la tabla segun convenga
-                $sql = "SELECT * FROM users";
+                $sql = "SELECT * FROM admin";
     
                 if(selectSQL($connection, $sql, $result)) {
                     // Estas variables sirven para cambiar el mensaje de error si no se encuentra
@@ -85,11 +85,58 @@
                                 // Se crean las variables de sesión añadir las que hagan falta
                                 $_SESSION['email'] = $row['email'];
                                 $_SESSION['password'] = $row['password'];
-                                $_SESSION['role'] = $row['role'];
-
+                                $_SESSION['role'] = "A";
                                 header('Location: index.php');
                             }
                         }
+                    }
+                    if ($found == false) {
+                        $sql = "SELECT * FROM teacher";
+    
+                        if(selectSQL($connection, $sql, $result)) {
+                            // Estas variables sirven para cambiar el mensaje de error si no se encuentra
+                            $found = false;
+                            $valid = false;
+            
+                            foreach ($result as $row){
+                                // Cambiar email por la clave primaria que se necesite
+                                if ($row['email'] == $_POST['email']){
+                                    $found = true;
+            
+                                    if($row['password'] == md5($_POST['password'])){
+                                        $valid = true;
+                                        // Se crean las variables de sesión añadir las que hagan falta
+                                        $_SESSION['email'] = $row['email'];
+                                        $_SESSION['password'] = $row['password'];
+                                        $_SESSION['role'] = "T";
+                                        header('Location: index.php');
+                                    }
+                                }
+                            }
+                    }
+                    if ($found == false) {
+                        $sql = "SELECT * FROM student";
+    
+                        if(selectSQL($connection, $sql, $result)) {
+                            // Estas variables sirven para cambiar el mensaje de error si no se encuentra
+                            $found = false;
+                            $valid = false;
+            
+                            foreach ($result as $row){
+                                // Cambiar email por la clave primaria que se necesite
+                                if ($row['email'] == $_POST['email']){
+                                    $found = true;
+            
+                                    if($row['password'] == md5($_POST['password'])){
+                                        $valid = true;
+                                        // Se crean las variables de sesión añadir las que hagan falta
+                                        $_SESSION['email'] = $row['email'];
+                                        $_SESSION['password'] = $row['password'];
+                                        $_SESSION['role'] = "S";
+                                        header('Location: index.php');
+                                    }
+                                }
+                            }
                     }
                     if ($found==false) echo '<script>alert("Username does not exist");</script>';
                     else if ($valid==false) echo '<script>alert("Wrong password");</script>';
