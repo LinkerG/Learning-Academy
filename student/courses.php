873ecadf3464acd5354942949df1c98c96dcd4b6
alexcede <alex.cedeno@inslapineda.cat>
2023-09-24 20:23:11 +0200

    cursos de estudiante casi acabado

--- student/courses.php
+++ student/courses.php
@@ -7,10 +7,18 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/style.css">
     <title>Student courses</title>
 </head>
 <body>
     <?php
+        if(!isset($_SESSION['role']) || $_SESSION['role'] != "S") {
+            include("needStudent.html");
+            header("Refresh: 5; URL='close.php'");
+            exit;
+        } else {
+            printHeader();
+        }   
         echo "<h1>Welcome " . $_SESSION['name'] . " " . $_SESSION['surname'] . "</h1>";
         echo "<h2>This are your courses right now</h2>";
     ?>
@@ -24,17 +32,20 @@
         </tr>
         <?php
             if(connectBD("learningacademy", $connection)) {
-                $sql = "SELECT * FROM course c INNER JOIN matriculates m ON c.courseId = m.courseId INNER JOIN student s ON s.dniStudent = m.dniStudent WHERE s.dniStudent = '" . $_SESSION['dniStudent'] . "' ;";
+                $sql = "SELECT m.task1,m.task2,m.task3,m.task4,m.score,c.endDate,c.name,c.photoPath FROM matriculates m INNER JOIN course c ON c.courseId = m.courseId INNER JOIN student s ON s.dniStudent = m.dniStudent WHERE s.dniStudent = '" . $_SESSION['dniStudent'] . "' ;";
             }
                 if(selectSQL($connection, $sql, $result)){
                     if(empty($result)) {
                         echo "<div>";
                         echo "<h1>You aren't in any course</h1>";
-                        echo "<a src='../courses.php'><h2>enter on any of our courses!</h2></a>";
+                        echo "<a src=".'../courses.php'.">enter on any of our courses!</a>";
                         echo "</div>";
-                        echo "<a>";
                     } else {
-                        print_r($result);
+                        foreach ($result as $course){
+                            echo"<div class="."course".">";
+                            echo "";
+                            echo "</div>";
+                        }
                     }
                 }
             
