833bed935d6e4502b0e5f1ad0f2ca45fb1c7680b
Alex Cedeño <alex.cedeno@inslapineda.cat>
2023-10-01 17:53:21 +0200

    Editar alumno acabado
    Falta pasar datos por json

--- student/profile.php
+++ student/profile.php
@@ -12,11 +12,12 @@
         <?php
             include ("../functions.php");
             printHeader();
-            if($_SERVER["REQUEST_METHOD"] == "POST"){
-                if(connectBD("learningacademy",$connection)){
-                    if($_FILES['photoPath']['error'] == 4){
-                        $_POST['photoPath']="/Learning-Academy/img/profilePhotos/default.png";
-                    }else{
+            if ($_SERVER["REQUEST_METHOD"] == "POST") {
+                header("Content-Type: application/json");
+                if (connectBD("learningacademy", $connection)) {
+                    if ($_FILES['photoPath']['error'] == 4) {
+                        $_POST['photoPath'] = "/Learning-Academy/img/profilePhotos/default.png";
+                    } else {
                         $_POST['photoPath'] = uploadPhoto(2);
                     }
                     $_SESSION['name'] = $_POST['name'];
@@ -26,42 +27,41 @@
                     $_SESSION['birthDate'] = $_POST['birthDate'];
                     $_SESSION['dniStudent'] = $_POST['dniStudent'];
                     $sql = "UPDATE student SET dniStudent = {$_POST['dniStudent']}, name = {$_POST['name']}, surname = {$_POST['surname']}, email = {$_POST['email']}, photoPath = {$_POST['photoPath']}, birthDate = {$_POST['birthDate']} WHERE dniStudent = {$_SESSION['dniStudent']};";
-                    updateSQL($connection,$sql);
+                    updateSQL($connection, $sql);
                     $reply = ["MSJ" => "Updated correctly!"];
-                    header("Content-Type: application/json");
                     echo json_encode($reply);
                     exit;
                 }
             }
             ?>
                 <div id="studentProfile">
-                    <div id="profileName">Name: <?php echo "{$_SESSION['name']}";?></div>
-                    <div id="profileSurname">Surname: <?php echo "{$_SESSION['surname']}";?></div>
-                    <div id="profileEmail">Email: <?php echo "{$_SESSION['email']}"; ?></div>
-                    <div id="profilePhoto"><img src="<?php echo "{$_SESSION['photoPath']}";?>"></div>
-                    <div id="profileBirthDate">Birth date: <?php echo "{$_SESSION['birthDate']}";?></div>
-                    <div id="profileStudentDni">DNI: <?php echo "{$_SESSION['dniStudent']}";?></div>
+                    <div class="profile-element" id="profileName">Name: <?php echo "{$_SESSION['name']}";?></div>
+                    <div class="profile-element" id="profileSurname">Surname: <?php echo "{$_SESSION['surname']}";?></div>
+                    <div class="profile-element" id="profileEmail">Email: <?php echo "{$_SESSION['email']}"; ?></div>
+                    <div class="profile-element" id="profilePhoto"><img src="<?php echo "{$_SESSION['photoPath']}";?>"></div>
+                    <div class="profile-element" id="profileBirthDate">Birth date: <?php echo "{$_SESSION['birthDate']}";?></div>
+                    <div class="profile-element" id="profileStudentDni">DNI: <?php echo "{$_SESSION['dniStudent']}";?></div>
                     <div class="btn"><button type="button" id="edit-btn" onclick="editProfile()">Edit profile</button></div>
                 </div>
                 <form enctype="multipart/form-data" id="profile-form" style="display:none;">
 
-                    <label for="name">Name: </label>
-                    <input type="text" id="name-input" name="name" value="<?php echo $_SESSION['name'] ?>"></input>
+                    <label for="name-input">Name: </label>
+                    <input type="text" class="form-element" id="name-input" name="name" value="<?php echo $_SESSION['name'] ?>"></input>
 
-                    <label for="surname">Surname: </label>
-                    <input type="text" id="surname-input" name="surname" value="<?php echo $_SESSION['surname']?>"></input>
+                    <label for="surname-input">Surname: </label>
+                    <input type="text" class="form-element" id="surname-input" name="surname" value="<?php echo $_SESSION['surname']?>"></input>
 
-                    <label for="email">Email: </label>
-                    <input type="text" id="email-input" name="email" value="<?php echo $_SESSION['email']?>"></input>
+                    <label for="email-input">Email: </label>
+                    <input type="text" class="form-element" id="email-input" name="email" value="<?php echo $_SESSION['email']?>"></input>
 
-                    <label for="photoPath">Photo: </label>
-                    <input type="file" id="photoPath-input" name="photoPath" value="<?php echo $_SESSION['photoPath']?>"></input>
+                    <label for="photoPath-input">Photo: </label>
+                    <input type="file" class="form-element" id="photoPath-input" name="photoPath" value="<?php echo $_SESSION['photoPath']?>"></input>
 
-                    <label for="birthDate">Birth date: </label>
-                    <input type="date" id="birthDate-input" name="birthDate" value="<?php echo $_SESSION['birthDate']?>"></input>
+                    <label for="birthDate-input">Birth date: </label>
+                    <input type="date" class="form-element" id="birthDate-input" name="birthDate" value="<?php echo $_SESSION['birthDate']?>"></input>
 
-                    <label for="dniStudent">Dni: </label>
-                    <input type="text" id="dni-input" name="dniStudent" value="<?php echo $_SESSION['dniStudent'] ?>" readonly></input>
+                    <label for="dni-input">Dni: </label>
+                    <input type="text" class="form-element" id="dni-input" name="dniStudent" value="<?php echo $_SESSION['dniStudent'] ?>" readonly></input>
 
                     <button type="button" id="save-btn" onclick="editProfile()">Update</button>
                 </form>
