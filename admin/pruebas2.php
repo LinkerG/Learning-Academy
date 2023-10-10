2093459a8d6abdd952d02e7f419f75e8c66b7b55
Iker Gonzalez Tirado <linkervideogames@gmail.com>
2023-10-10 23:08:19 +0200

    He tardado 15 minutos

--- admin/pruebas2.php
+++ admin/pruebas2.php
@@ -1,6 +1,7 @@
 <?php
 if(isset($_POST)){
     echo "SIUUU";
+    print_r($_POST);
     $dataJSON = file_get_contents('php://input');
     $arrayData = json_decode($dataJSON, true);
     print_r($arrayData);
